444ea3cfd68928ddf93ea7f3c0ca78c9617839a8
PavelUshakov03 <p.ushakov.2003@gmail.com>
2021-10-22 22:47:46 +0300

    Initial Commit

---
+++ Shared/TicTacToeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeAppApp.swift
+//  Shared
+//
+//  Created by Pavel Ushakov on 22.10.21.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
